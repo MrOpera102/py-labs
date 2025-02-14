b70b0c4b10abcab6013220f04248af4a26246b78
MrOpera102 <vlad051082@gmail.com>
2025-02-14 19:37:32 +0300

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,36 +1,29 @@
 BOOKS_DATABASE = [{"id": 1, "name": "test_name_1", "pages": 200, }, {"id": 2, "name": "test_name_2", "pages": 400, }]
 
-
 class Book:
     def __init__(self, id_: int, name: str, pages: int):
         self.id = id_
         self.name = name
         self.pages = pages
-
     def __str__(self):
         return f'Книга "{self.name}"'
-
     def __repr__(self):
         return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
-
 class Library:
     def __init__(self, books=None):
         if books is None:
             self.books = []
         else:
             self.books = books
-
     def get_next_book_id(self):
         if not self.books:
             return 1
         return self.books[-1].id + 1
-
     def get_index_by_book_id(self, id_):
         for index, book in enumerate(self.books):
             if book.id == id_:
                 return index
         raise ValueError("Книги с запрашиваемым id не существует")
-
 if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
